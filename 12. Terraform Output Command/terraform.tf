2ce2d36fc5646fa4e5fa5117171955edd89ea379
Mangesh <m.sonawanegot@gmail.com>
2026-01-29 23:09:15 +0530

    Now its Working

--- 12. Terraform Output Command/terraform.tf
+++ 12. Terraform Output Command/terraform.tf
@@ -4,6 +4,29 @@ resource "github_repository" "terraform-output-command" {
   auto_init = true
 }
 
-output "repourl" {
+output "repo_url" {
   value = github_repository.terraform-output-command.html_url
 }
+
+// Commands to run the Terraform script
+// $ terraform init
+// $ terraform validate 
+// $ terraform plan
+// $ terraform apply -auto-approve
+
+// To get the output value html_url
+// now use $ terraform plan command by change output lablel from repourl to repo_url
+// It will show the changes in output
+// Example output after running terraform plan command
+// OUTPUT :
+// github_repository.terraform-output-command: Refreshing state... [id=terraform-output-command]
+
+// Changes to Outputs:
+  // + repo_url = "https://github.com/MangeshGot/terraform-output-command"
+  // - repourl  = "https://github.com/MangeshGot/terraform-output-command" -> null
+
+// Finally run the command to get the output value
+// $ terraform output repo_url
+// It will return the output value
+// OUTPUT :
+// https://github.com/MangeshGot/terraform-output-command
